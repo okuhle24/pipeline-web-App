0c3d3878f3b3ae633cee3804c807cbd43e8bcb01
OkuhleNakile <OkuhleNakile@DESKTOP-FDIR3LM>
2024-10-24 11:25:29 +0200

    Add project files.

---
+++ pipelinewebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pipelinewebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
